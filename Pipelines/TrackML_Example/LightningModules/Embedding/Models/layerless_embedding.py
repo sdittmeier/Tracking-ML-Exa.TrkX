2fa76d6299cbb26f72181b1ec25885807389b3d2
SachinGupta7 <sguptage1245@gmail.com>
2022-11-16 18:36:29 +0100

    Added function for creatin qmlp

--- Pipelines/TrackML_Example/LightningModules/Embedding/Models/layerless_embedding.py
+++ Pipelines/TrackML_Example/LightningModules/Embedding/Models/layerless_embedding.py
@@ -14,7 +14,7 @@ import torch
 from torch_geometric.loader import DataLoader
 
 # Local imports
-from ..utils import graph_intersection, make_mlp
+from ..utils import graph_intersection, make_mlp,make_quantized_mlp
 
 
 class LayerlessEmbedding(EmbeddingBase):
@@ -25,14 +25,32 @@ class LayerlessEmbedding(EmbeddingBase):
         """
         # Construct the MLP architecture
         in_channels = hparams["spatial_channels"] + hparams["cell_channels"]
+      
+        if(hparams["quantized_network"]==False):
+            
+            self.network = make_mlp(
+                in_channels,
+                [hparams["emb_hidden"]] * hparams["nb_layer"] + [hparams["emb_dim"]],
+                hidden_activation=hparams["activation"],
+                output_activation=None,
+                layer_norm=True,
+            )
+        else:
 
-        self.network = make_mlp(
-            in_channels,
-            [hparams["emb_hidden"]] * hparams["nb_layer"] + [hparams["emb_dim"]],
-            hidden_activation=hparams["activation"],
-            output_activation=None,
-            layer_norm=True,
-        )
+            self.network = make_quantized_mlp(
+                in_channels,
+                [hparams["emb_hidden"]] * hparams["nb_layer"] + [hparams["emb_dim"]],
+                hidden_activation=hparams["hidden_activation_qnn"],
+                output_activation=hparams["output_activation_qnn"],
+                weight_bit_width = hparams["weight_bit_width"],
+                activation_bit_width = hparams["activation_bit_width"],
+                input_layer_quantization = hparams["input_layer_quantization"],
+                layer_norm = True
+
+            )
+
+
+
 
         self.save_hyperparameters()
 
